305fb0f1349d962b36ad2c7fc8dc50bfbbdc9d3b
jeeva-l <jeevaloganathan001@gmail.com>
2025-04-13 12:43:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const accessKey="prH9C2yGXFdOgtTTIPUk-av5b0GOoUxgaGv2ffMo36k";
+
+const SearchForm = document.getElementById("search-form");
+const SearchBox = document.getElementById("search-box");
+const SearchResult = document.getElementById("search-result");
+const ShowMoreBtn = document.getElementById("show-more-btn");
+
+let keywords = "";
+let page = 1;
+
+async function searchImages() {
+    keywords = SearchBox.value;
+    const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keywords}&client_id=${accessKey}&per_page=12`;
+
+    const response=await fetch(url);
+    const data=await response.json();
+
+    if(page === 1){
+        SearchResult.innerHTML="";
+    }
+    const results=data.results;
+
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        SearchResult.appendChild(imageLink);
+    })
+    ShowMoreBtn.style.display="block";
+}
+SearchForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    page =  1;
+    searchImages();
+})
+
+ShowMoreBtn.addEventListener("click",()=>{
+    page++;
+    searchImages();
+})
